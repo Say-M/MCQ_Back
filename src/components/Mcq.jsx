ec01e35af12454885ad85328e4b7c48625e140e2
Mohammad Sayem <msayem089@gmail.com>
2020-10-20 15:18:51 +0600

    MCQ fetch

--- src/components/Mcq.jsx
+++ src/components/Mcq.jsx
@@ -4,7 +4,7 @@ import db from "./firebase_config";
 
 const Mcq = () => {
     const { id } = useParams()
-    const [question, setQuestion] = useState([[]])
+    const [questions, setQuestions] = useState([{ mcq: [] }])
     useEffect(() => {
         let arr = [];
         const snap = db
@@ -15,17 +15,29 @@ const Mcq = () => {
                 snap.forEach((d) => {
                     arr.push(d.data());
                 });
-                setQuestion(arr);
+                setQuestions(arr);
             });
     }, [id]);
 
     return <>
         <div className="container">
+            <h2 className="text-center my-3">{questions[0].title}</h2>
+            {questions[0].category === "Admission" ? <h3 className="text-center">{questions[0].university})</h3> : null}
             <div className="row">
-                {console.log(question[0].mcq)}
-                {/* {question[0].mcq.map((que, i) => {
-                    return <div key={i} className="form-group col-md-6">
-                        <h3>{que.question}</h3>
+                <div className="col-6">
+                    <p><strong>Total questions:</strong> {questions[0].total}</p>
+                    <p><strong>Duration:</strong> {questions[0].duration} {questions[0].durationCondition}</p>
+                </div>
+                <div className="col-6 text-right">
+                    <p><strong>Duration:</strong> {questions[0].category}</p>
+                    <p><strong>Marks:</strong> {questions[0].percent}</p>
+                </div>
+            </div>
+            <div className="row border">
+                {console.log(questions[0])}
+                {questions[0].mcq.map((que, i) => {
+                    return <div key={i} className="my-3 col-md-6">
+                        <strong><p>{que.question}</p></strong>
                         <div className="row">
                             {que.options.map((opt, i) => {
                                 return <div key={i} className="col-sm-6 mb-2"><strong>{i + 1}:</strong> {opt}</div>
@@ -33,7 +45,7 @@ const Mcq = () => {
                             <div className="col-12"><strong>Answer:</strong> {que.rightAnswer}</div>
                         </div>
                     </div>
-                })} */}
+                })}
                 {/* <div className="form-group col-md-6">
                     <h3>Title</h3>
                     <div className="row">
